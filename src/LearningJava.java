700d37161dcd948e2b3cd3138b91550c47e3bac3
reformedhope <steph_salzburg@hotmail.com>
2023-11-20 18:26:15 -0500

    Learning some Java

---
+++ src/LearningJava.java
@@ -0,0 +1,7 @@
+public class LearningJava {
+    public static void main(String[] args){
+//        System.out.println("hello world");
+        string words = "hello there";
+        System.out.println();
+    }
+}
